76e9628bbf0c1aec7538fdef7f2d1ada11edde50
João Herrera <joaogherrera@hotmail.com>
2023-01-28 23:39:30 -0300

    Deploy final

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joaog\OneDrive\Documentos\Meu portfólio\venv
+set VIRTUAL_ENV=C:\Users\joaog\OneDrive\Documentos\Projetos git\Projetos no ar\Meu portfólio\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
